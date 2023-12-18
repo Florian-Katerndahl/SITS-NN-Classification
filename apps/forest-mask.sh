89f02482e6579aa34e92323ca0ae4121e0acae3c
Florian Katerndahl <florian@katerndahl.com>
2023-12-18 16:05:17 +0000

    misc changes to now unsused shell script

--- apps/forest-mask.sh
+++ apps/forest-mask.sh
@@ -1,5 +1,6 @@
 # TODO script currently only works when in Eolab folder, but this placement doesn't make sense.
-# Should be incorporated into NF workflow anyway
+# TODO Don't hardcode blocksizes in last step
+# Should be incorporated into NF workflow!
 set -e
 
 docker run --rm -v $PWD:/lol ghcr.io/osgeo/gdal:alpine-normal-latest \
@@ -13,7 +14,7 @@ UPPER_LATITUDE=$(bc  <<< "x=$(echo $BBOX | cut -d ' ' -f4)/1;x-(x%3)")
 
 for ((i=LOWER_LATITUDE; i<=UPPER_LATITUDE; i+=3)); do
   for ((j=LOWER_LONGITUDE; j<=UPPER_LONGITUDE; j+=3)); do
-    #! THIS ONLY WORKS FOR THE NOTHERN HEMISPHERE, EAST OF GREENWICH
+    #! THIS ONLY WORKS FOR THE NOTHERN HEMISPHERE, EAST OF GREENWICH => fixed in groovy closure
     printf "ESA_WorldCover_10m_2020_v100_N%.2dE%.3d_Map.tif\n" $i $j >> ./world-cover.txt
   done
 done
@@ -28,8 +29,7 @@ while read -r line; do
     bash -c 'force-cube -s 10 -n 0 -o /data/masks/ -b "worldcover" -j 8 "/worldcover/${LINE}"'
 done < ./world-cover.txt
 
-# TODO Don't hardcode blocksizes
 find Eolab/masks -name '*tif' -type f -execdir \
   gdal_calc.py --outfile=mask.tif --calc="1*logical_and(A>=10,A<20)" \
   --type=Byte --NoDataValue=0 --creation-option="BLOCKXSIZE=300" --creation-option="BLOCKYSIZE=3000" \
-  --overwrite -A {} +
+  --creation-option="COMPRESS=LZW" --creation-option="PREDICTOR=2" --overwrite -A {} +
